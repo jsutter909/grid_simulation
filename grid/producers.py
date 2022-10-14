308396db5a588255da2f4192738d94fbfb0a7b0c
Jake Sutter <jake@pop-os.localdomain>
2022-10-14 01:42:11 -0500

    more polishing

--- grid/producers.py
+++ grid/producers.py
@@ -1,11 +1,9 @@
 import abc
 import math
 from dataclasses import dataclass
+from grid.grid_market import get_solar_factor
 
 from grid.parameters import Time, Weather, PowerRange
-
-SOLAR_START_TIME = Time(6, 0)
-SOLAR_END_TIME = Time(18, 0)
 
 
 class Producer(abc.ABC):
@@ -31,22 +29,5 @@ class SolarPlant(Producer):
 
     def get_power_range(self, weather: Weather, time: Time) -> PowerRange:
         return PowerRange(
-            min_power=0, max_power=self.capacity * weather.cloud_cover * SolarPlant.get_solar_factor(time)
+            min_power=0, max_power=self.capacity * weather.cloud_cover * get_solar_factor(time)
         )
-
-    @classmethod
-    def get_solar_factor(cls, time: Time) -> float:
-        """
-        Computes intensity of solar power on a location given the time of day.
-        :param time The time of day
-        :return: The intensity of the solar power
-        """
-
-        start_time_minutes = SOLAR_START_TIME.time_to_minutes()
-        end_time_minutes = SOLAR_END_TIME.time_to_minutes()
-        current_time_minutes = time.time_to_minutes()
-
-        # will be a float in [0, 1]
-        scaled_time = (current_time_minutes - start_time_minutes) / (end_time_minutes - start_time_minutes)
-
-        return math.sin(scaled_time * math.pi)
