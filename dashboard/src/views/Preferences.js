2488050ee1d92c8d5b644da40ae14e8d3f8c724a
Ryan McPhail <mcphailryan1@gmail.com>
2022-11-09 00:46:36 -0600

    finishing up preferences tab

--- dashboard/src/views/Preferences.js
+++ dashboard/src/views/Preferences.js
@@ -15,13 +15,24 @@ import {
   OverlayTrigger,
   Tooltip,
 } from "react-bootstrap";
-import ToggleButton from '@mui/material/ToggleButton';
-import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
+import TextField from "@mui/material/TextField";
+import ToggleButton from "@mui/material/ToggleButton";
+import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
+import FormControl from "@mui/material/FormControl";
+import InputLabel from "@mui/material/InputLabel";
+import FilledInput from "@mui/material/FilledInput";
+import InputAdornment from "@mui/material/InputAdornment";
+import { TimePicker } from "@mui/x-date-pickers/TimePicker";
+import dayjs from "dayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 
 function Preferences() {
-  const [alignment, setAlignment] = useState('web');
-  const [alignment1, setAlignment1] = useState('solar');
-  const [alignment2, setAlignment2] = useState('coal');
+  //consts for the AI preferences
+  const [alignment, setAlignment] = useState("android");
+  const [alignment1, setAlignment1] = useState("solar");
+  const [alignment2, setAlignment2] = useState("enabled");
+  const [alignment3, setAlignment3] = useState("enable");
 
   const handleChange = (event, newAlignment) => {
     setAlignment(newAlignment);
@@ -31,6 +42,40 @@ function Preferences() {
   };
   const handleChange2 = (event, newAlignment2) => {
     setAlignment2(newAlignment2);
+  };
+  const handleChange3 = (event, newAlignment3) => {
+    setAlignment3(newAlignment3);
+  };
+
+  //consts for the user set preferences
+  const [valueTime, setValueTime] = useState(dayjs("2014-08-18T21:11:54"));
+  const [valueTime1, setValueTime1] = useState(dayjs("2014-08-18T21:11:54"));
+  const handleChangeTime = (newValue) => {
+    setValueTime(newValue);
+  };
+  const handleChangeTime1 = (newValue) => {
+    setValueTime1(newValue);
+  };
+
+  const [values, setValues] = useState({
+    amount: "",
+    password: "",
+    weight: "",
+    weightRange: "",
+    showPassword: false,
+  });
+  const [values1, setValues1] = useState({
+    amount: "",
+    password: "",
+    weight: "",
+    weightRange: "",
+    showPassword: false,
+  });
+  const handleChangeAmount = (prop) => (event) => {
+    setValues({ ...values, [prop]: event.target.value });
+  };
+  const handleChangeAmount1 = (prop) => (event) => {
+    setValues1({ ...values1, [prop]: event.target.value });
   };
 
   return (
@@ -139,7 +184,58 @@ function Preferences() {
                 <p className="card-category">Toggle to change</p>
               </Card.Header>
               <Card.Body>
-                <div></div>
+                <div>
+                  <h6>set auto-buy</h6>
+                  <FormControl sx={{ m: 1 }} variant="filled">
+                    <InputLabel htmlFor="filled-adornment-amount">
+                      Amount
+                    </InputLabel>
+                    <FilledInput
+                      id="filled-adornment-amount"
+                      value={values.amount}
+                      onChange={handleChangeAmount("amount")}
+                      startAdornment={
+                        <InputAdornment position="start">$</InputAdornment>
+                      }
+                    />
+                  </FormControl>
+                </div>
+                <div style={{ marginTop: "10px" }}>
+                  <h6>set auto-sell</h6>
+                  <FormControl sx={{ m: 1 }} variant="filled">
+                    <InputLabel htmlFor="filled-adornment-amount">
+                      Amount
+                    </InputLabel>
+                    <FilledInput
+                      id="filled-adornment-amount"
+                      value={values1.amount}
+                      onChange={handleChangeAmount1("amount")}
+                      startAdornment={
+                        <InputAdornment position="start">$</InputAdornment>
+                      }
+                    />
+                  </FormControl>
+                </div>
+                <div style={{ marginTop: "10px" }}>
+                  <LocalizationProvider dateAdapter={AdapterDayjs}>
+                    <h6>time for battery top off</h6>
+                    <TimePicker
+                      value={valueTime}
+                      onChange={handleChangeTime}
+                      renderInput={(params) => <TextField {...params} />}
+                    />
+                  </LocalizationProvider>
+                </div>
+                <div style={{ marginTop: "10px" }}>
+                  <LocalizationProvider dateAdapter={AdapterDayjs}>
+                    <h6>time for car charging</h6>
+                    <TimePicker
+                      value={valueTime1}
+                      onChange={handleChangeTime1}
+                      renderInput={(params) => <TextField {...params} />}
+                    />
+                  </LocalizationProvider>
+                </div>
               </Card.Body>
               <Card.Footer>
                 <hr></hr>
@@ -158,7 +254,33 @@ function Preferences() {
               </Card.Header>
               <Card.Body>
                 <div>
-                  <h6>Battery Refill Strategy</h6>
+                  <h6>Predict cloudiness and Extreme Weather</h6>
+                  <ToggleButtonGroup
+                    color="primary"
+                    value={alignment2}
+                    exclusive
+                    onChange={handleChange2}
+                    aria-label="Platform"
+                  >
+                    <ToggleButton value="enabled">Enabled</ToggleButton>
+                    <ToggleButton value="disabled">Disabled</ToggleButton>
+                  </ToggleButtonGroup>
+                </div>
+                <div>
+                  <h6 style={{ marginTop: "10px" }}>predict car charging</h6>
+                  <ToggleButtonGroup
+                    color="primary"
+                    value={alignment3}
+                    exclusive
+                    onChange={handleChange3}
+                    aria-label="Platform"
+                  >
+                    <ToggleButton value="enable">Enabled</ToggleButton>
+                    <ToggleButton value="disable">disabled</ToggleButton>
+                  </ToggleButtonGroup>
+                </div>
+                <div>
+                  <h6 style={{ marginTop: "10px" }}>Battery Refill Strategy</h6>
                   <ToggleButtonGroup
                     color="primary"
                     value={alignment}
@@ -171,7 +293,7 @@ function Preferences() {
                     <ToggleButton value="ios">Aggressive</ToggleButton>
                   </ToggleButtonGroup>
                 </div>
-                <div style={{marginTop: "10px"}}>
+                <div style={{ marginTop: "10px" }}>
                   <h6>Energy Preference</h6>
                   <ToggleButtonGroup
                     color="primary"
